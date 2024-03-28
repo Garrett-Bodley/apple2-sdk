836b912c8eec754e70eaf2b2e28edb78f2cdfea2
Garrett Bodley <garrett.bodley@gmail.com>
2024-03-28 00:38:01 -0400

    Improve comments.

--- src/main.c
+++ src/main.c
@@ -3,9 +3,12 @@
 
 int main(void)
 {
-  printf("Hello, World!");
-  // This matters for compiling to disk image
-  // If you don't add an infinite loop it will 
+  printf("Hello, World!\n");
+
+  // This matters when compiling to a disk image
+  //
+  // If you don't add an infinite loop the emulator will quickly execute
+  // and exit your program before you're able to see the output.
   while(true);
   return 0;
 }
